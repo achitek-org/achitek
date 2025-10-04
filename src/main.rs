7b65e18ed32bfca8c2e11deba01599a60e0417f4
Samuel Joli <samuel.joli.ftn@gmail.com>
2025-10-04 16:22:18 -0400

    refactor: clap implementation to use derive interface

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,66 @@
-use clap::{
-    crate_authors, crate_description, crate_name, crate_version, Arg, ArgAction, ArgMatches,
-    Command,
-};
+use clap::{Parser, Subcommand};
 use env_logger::Builder;
 use kopye::api::KopyeError;
 use log::LevelFilter;
 use miette::Result as MietteResult;
-use std::env;
+
+#[derive(Debug, Clone, Parser)]
+#[command(about, version)]
+struct Cli {
+    #[command(subcommand)]
+    commands: Commands,
+    #[arg(long, short, global = true, help = "Enable verbose output")]
+    // .action(ArgAction::SetTrue)
+    verbose: bool,
+}
+
+#[derive(Debug, Clone, Subcommand)]
+enum Commands {
+    #[command(about = "Copies a template from a repo reference to a destination")]
+    Copy {
+        #[arg(
+            help = "git repository reference where templates live",
+            required = true
+        )]
+        repo: String,
+        #[arg(help = "template name", required = true)]
+        template: String,
+        #[arg(
+            help = "The destination directory where the project will be created",
+            required = true
+        )]
+        destination: String,
+    },
+    #[command(about = "list template")]
+    List {
+        #[arg(
+            help = "git repository reference where templates live", // <--------- OR local dir
+            required = true
+        )]
+        repo: String,
+    },
+}
 
 fn main() -> MietteResult<()> {
-    let matches = Command::new(crate_name!())
-        .about(crate_description!())
-        .author(crate_authors!())
-        .version(crate_version!())
-        .arg(
-            Arg::new("verbose")
-                .short('v')
-                .long("verbose")
-                .help("Enable verbose output")
-                .action(ArgAction::SetTrue)
-                .global(true),
-        )
-        .subcommand(
-            Command::new("copy")
-                .about("Copies a template from a repo reference to a destination")
-                .arg(
-                    Arg::new("repo")
-                        .help("git repository reference where templates live")
-                        .required(true),
-                )
-                .arg(Arg::new("template").help("template name").required(true))
-                .arg(
-                    Arg::new("destination")
-                        .help("The destination directory where the project will be created")
-                        .required(true),
-                ),
-        )
-        .subcommand(
-            Command::new("list")
-                .about("list templates")
-                .arg(Arg::new("repo").help("git repository reference where templates live")),
-        )
-        .get_matches();
+    let args = Cli::parse();
 
-    let is_verbose = matches.get_flag("verbose");
+    init_logger(args.verbose);
 
-    init_logger(is_verbose);
-
-    match matches.subcommand() {
-        Some(("copy", args)) => {
-            handle_copy(args).map_err(miette::Report::new)?;
+    match args.commands {
+        Commands::Copy {
+            repo,
+            template,
+            destination,
+        } => {
+            handle_copy(repo, template, destination).map_err(miette::Report::new)?;
 
             Ok(())
         }
-        Some(("list", args)) => {
-            handle_list(args).map_err(miette::Report::new)?;
+        Commands::List { repo } => {
+            handle_list(repo).map_err(miette::Report::new)?;
 
             Ok(())
         }
-        _ => unreachable!(),
     }
 }
 
@@ -76,20 +78,10 @@ fn init_logger(verbose: bool) {
     builder.init();
 }
 
-fn handle_copy(args: &ArgMatches) -> Result<(), KopyeError> {
-    let repo = args.get_one::<String>("repo").expect("repo required");
-    let template_name = args
-        .get_one::<String>("template")
-        .expect("template required");
-    let destination = args
-        .get_one::<String>("destination")
-        .expect("destination expected");
-
-    kopye::api::copy_template(repo, template_name, destination)
+fn handle_copy(repo: String, template: String, destination: String) -> Result<(), KopyeError> {
+    kopye::api::copy_template(&repo, &template, &destination)
 }
 
-fn handle_list(args: &ArgMatches) -> Result<(), KopyeError> {
-    let repo = args.get_one::<String>("repo").expect("repo required");
-
-    kopye::api::list_templates(repo)
+fn handle_list(repo: String) -> Result<(), KopyeError> {
+    kopye::api::list_templates(&repo)
 }
